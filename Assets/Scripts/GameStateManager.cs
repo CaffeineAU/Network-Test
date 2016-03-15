9b49d6836c6b51dc676548332bafc4a8f619c77a
Liam O'Hagan <caffeineau@gmail.com>
2016-03-15 21:46:34 +1100

    Simple network sync of player 'turn'

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+public class GameStateManager : NetworkManager {
+
+
+    private List<PlayerManager> Players = new List<PlayerManager>();
+
+
+    public int turn = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.T)) // change turn
+        {
+            ChangeTurn();
+
+        }
+    }
+
+    
+    private void ChangeTurn()
+    {
+        if (Players.Count > 0) // only on the server
+        {
+            turn++;
+            turn = turn % Players.Count;
+            foreach (PlayerManager player in Players)
+            {
+                player.isMyTurn = false;
+            }
+                Debug.Log("It's " + Players[turn].name + "'s turn");
+            Players[turn].isMyTurn = true;
+
+        }
+    }
+
+    // called when a new player is added for a client
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        var player = (GameObject)GameObject.Instantiate(playerPrefab, RandomPosition(), Quaternion.identity);
+        Players.Add(player.GetComponent<PlayerManager>());
+        Players[Players.Count-1].name = Players.Count.ToString();
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+
+    Vector3 RandomPosition()
+    {
+        return new Vector3(Random.Range(-10, 10), 0.5f, Random.Range(-10, 10));
+    }
+
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerManager : NetworkBehaviour {
+
+
+    [SyncVar(hook = "OnSyncTurnHook")]
+    public bool isMyTurn;
+
+
+    void OnSyncTurnHook(bool value)
+    {
+        if (isMyTurn != value)
+        {
+            Debug.Log("I'm " + name + " and it's " + (value ? "my turn" : "not my turn"));
+
+        }
+            isMyTurn = value;
+            GetComponentInParent<MeshRenderer>().material = (isMyTurn ? highlightMaterial : normalMaterial);
+    }
+
+    public Material highlightMaterial;
+    public Material normalMaterial;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
